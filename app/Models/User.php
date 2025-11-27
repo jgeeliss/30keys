36b8591dc5fe44768f5ba64df31bd2415e8ff92b
Jesse Geelissen <65621046+jgeeliss@users.noreply.github.com>
2025-11-27 21:54:59 +0100

    bugfix - I use user_alias i/o name, need to change al the references!

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -18,7 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'user_alias',
         'email',
         'password',
     ];
@@ -45,8 +46,6 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-
 
     public function keyboards(): BelongsToMany
     {
