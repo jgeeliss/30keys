befef7bd90ee22d5a5ac0de85b0a9fd1cf6b9567
Jesse Geelissen <jesse.geelissen@katholiekonderwijs.vlaanderen>
2025-11-30 21:23:10 +0100

    added top3 layouts to the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Keyboard;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        return view('home');
+        // Get keyboards with ratings and calculate average
+        $topKeyboards = Keyboard::with('ratings')
+            ->get()
+            // note:fn() is used to create an arrow function
+            ->filter(fn($keyboard) => $keyboard->totalRatings() > 0)
+            ->sortByDesc(fn($keyboard) => $keyboard->averageRating())
+            ->take(3);
+
+        return view('home', compact('topKeyboards'));
     }
-    // TODO: Add methods for additional home page functionality like showing latests layouts or something, i'm not sure yet.
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,20 @@
     <h1>
         Welcome to the Keyboard Layout Lounge Home Page!
     </h1>
+
+    @if($topKeyboards->isNotEmpty())
+        <section style="margin-top: 30px;">
+            <h2>Top Rated Layouts</h2>
+            @foreach($topKeyboards as $keyboard)
+                <div style="margin-bottom: 20px;">
+                    <h3>
+                        <a href="{{ route('keyboards.show', $keyboard) }}">{{ $keyboard->name }}</a>
+                        <span style="font-size: medium;">&nbsp;&nbsp;&nbsp;by {{ $keyboard->user->user_alias ?? 'Unknown' }}</span>
+                        <span style="font-size: medium; color: #666;">&nbsp;&nbsp;&nbsp;★ {{ number_format($keyboard->averageRating(), 1) }} ({{ $keyboard->totalRatings() }} {{ $keyboard->totalRatings() === 1 ? 'rating' : 'ratings' }})</span>
+                    </h3>
+                    @include('keyboards._layout', ['keyboard' => $keyboard])
+                </div>
+            @endforeach
+        </section>
+    @endif
 @endsection
