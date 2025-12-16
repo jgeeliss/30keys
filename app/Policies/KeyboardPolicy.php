27dbae9256060bbf4ea1727897df8f4691c8bb06
Jesse Geelissen <65621046+jgeeliss@users.noreply.github.com>
2025-12-16 21:20:08 +0100

    created KeyboardPolicy with update() and delete() methods

---
+++ app/Policies/KeyboardPolicy.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Keyboard;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+// note: createn by running "php artisan make:policy KeyboardPolicy --model=Keyboard"
+class KeyboardPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Keyboard $keyboard): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Keyboard $keyboard): bool
+    {
+        // Only the owner can update
+        return $user->id === $keyboard->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Keyboard $keyboard): bool
+    {
+        // Only the owner or admin can delete
+        return $user->id === $keyboard->user_id || $user->isAdmin();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Keyboard $keyboard): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Keyboard $keyboard): bool
+    {
+        return false;
+    }
+}
